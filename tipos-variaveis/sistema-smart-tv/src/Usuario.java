d0e6c2b2b45d1c6f625f157d8fc5e64ee061e0b4
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2023-04-21 21:40:14 -0300

    status

--- tipos-variaveis/sistema-smart-tv/src/Usuario.java
+++ tipos-variaveis/sistema-smart-tv/src/Usuario.java
@@ -2,10 +2,20 @@ public class Usuario {
     public static void main(String[] args) throws Exception {
         SmartTv smartTv = new SmartTv();
 
-        System.out.println("TV Ligada ?" + smartTv.ligada);
+        System.out.println("TV Ligada ? " + smartTv.ligada);
 
-        System.out.println("Canal Atual ?" + smartTv.canal);
+        System.out.println("Canal Atual ? : " + smartTv.canal);
 
-        System.out.println("Volume Atual ?" + smartTv.volume);
+        System.out.println("Volume Atual ? : " + smartTv.volume);
+
+        smartTv.ligar();
+
+        System.out.println("Novo Status -> TV Ligada ? " + smartTv.ligada);
+
+
+        smartTv.desligar();
+
+        System.out.println("Novo Status -> TV Ligada ? " + smartTv.ligada);
+
     }
 }
